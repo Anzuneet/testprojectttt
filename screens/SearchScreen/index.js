ba4801122f1638fe465670cec90e00286d8d8ffd
kojunsu <xunswk@gmail.com>
2018-02-19 11:48:49 +0900

    커밋테스트

--- screens/SearchScreen/index.js
+++ screens/SearchScreen/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {View, Text }from "react-native";
 
-const SearchScreen = props =><Text> search screen</Text>;
+const SearchScreen = props =><Text> search 22 screen</Text>;
 
 export default SearchScreen;
